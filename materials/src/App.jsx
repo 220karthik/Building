6559c975365234b459e8612e3377fd2e93270f83
Karthik <karthiksolaisamy6@gmail.com>
2025-05-09 09:24:13 +0530

    save: work before pulling from origin

--- materials/src/App.jsx
+++ materials/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './Signup.jsx';
 import ProductPage from './userdash.jsx';
 import CartPage from './cart.jsx';
 import AdminDashboard from './admindash.jsx';
+
 import Footer from './Footer.jsx';
 import { Route, Routes } from 'react-router-dom'
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/Products" element={<ProductPage />} />
         <Route path="/Cart" element={<CartPage />} />
         <Route path="/Admindash" element={<AdminDashboard />} />
+       
       </Routes>
       <Footer />
 
